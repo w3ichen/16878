125128daae76793ed0987f3c00631a22800b4dc8
Weichen Qiu <w3ichen@gmail.com>
2024-10-06 21:52:40 +0000

    use #define to select main() to call

--- main.cpp
+++ main.cpp
@@ -2,22 +2,68 @@
 #include "debug_mort.h"
 #include "nucleo_led.h"
 
+// --------------------------
+// Select code to run
+// #define HELLO_WORLD
+// #define PART1_Q1
+// #define PART1_Q2
+#define PART1_Q3
+// --------------------------
+
+
 void delay(int num){
     for (int i=0; i<num; i++){
         // busy wait
     }
 }
 
+
+#ifdef PART1_Q3
 int main (void)
 {
     /* Initializations */
     init_LED1(); //uncomment once you have filled in the function
     while(1)
     {
-        // toggle_LED1(); //uncomment once you have filled in the function
         set_LED1_to_C6();
-        // debugprintHelloWorld();
         delay(10000000);
     }
 }
+#endif
 
+#ifdef PART1_Q2
+int main (void)
+{
+    /* Initializations */
+    init_LED1(); //uncomment once you have filled in the function
+    while(1)
+    {
+        set_LED1_to_C6();
+        delay(10000000);
+    }
+}
+#endif
+
+#ifdef PART1_Q1
+int main (void)
+{
+    /* Initializations */
+    init_LED1(); //uncomment once you have filled in the function
+    while(1)
+    {
+        toggle_LED1(); //uncomment once you have filled in the function
+        delay(10000000);
+    }
+}
+#endif
+
+#ifdef HELLO_WORLD
+int main (void)
+{
+    while(1)
+    {
+        debugprintHelloWorld();
+        delay(10000000);
+    }
+}
+#endif
