cc3d3d963d0bc8a825f3705b31bf2d38ba1bdb3f
Weichen Qiu <w3ichen@gmail.com>
2024-10-28 16:33:41 +0000

    init lab5
    Signed-off-by: Weichen Qiu <w3ichen@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "debug_mort.h"
+#include "nucleo_led.h"
+
+int main (void)
+{
+    /* Initializations */
+    //init_LED1(); //uncomment once you have filled in the function
+    while(1)
+    {
+      
+      //toggle_LED1(); //uncomment once you have filled in the function
+      debugprintHelloWorld();
+    
+    }
+}
+
---
+++ nucleo_led.h
@@ -0,0 +1,26 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __LED1_H_
+#define __LED1_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Macros for Everyone--------------------------------------------------------*/
+
+
+
+
+/*Function definitions---------------------------------------------------------*/
+
+void init_LED1(void);
+void toggle_LED1( void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__LED1_H */
