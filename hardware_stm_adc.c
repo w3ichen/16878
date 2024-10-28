b7b1e89eb14041dda36e757643729abec666cff8
Weichen Qiu <w3ichen@gmail.com>
2024-10-28 16:39:38 +0000

    add new adc and dma files

---
+++ hardware_stm_adc.c
@@ -0,0 +1,1 @@
+#include "hardware_stm_adc.h"
---
+++ hardware_stm_dma_controller.h
@@ -0,0 +1,21 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __HARDWARE_STM_DMA_CONTROLLER_H_
+#define __HARDWARE_STM_DMA_CONTROLLER_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+
+/* Function definitions ---------------------------------------------------------*/
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__GPIO_H */
